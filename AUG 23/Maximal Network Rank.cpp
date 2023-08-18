25531fa0d429ffadcaf213b1970b148a8fdfe9e0
Prashant Dhote <prashantdhote400@gmail.com>
2023-08-18 18:12:31 +0530

    Create Maximal Network Rank.cpp

---
+++ AUG 23/Maximal Network Rank.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maximalNetworkRank(int n, vector<vector<int>>& roads) {
+        vector<unordered_set<int>>graph(n);
+
+        for(const auto& road:roads){
+            graph[road[0]].insert(road[1]);
+            graph[road[1]].insert(road[0]);
+        }
+
+        int maximal=0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int range=graph[i].size()+graph[j].size();
+
+                if(graph[j].count(i)){
+                    range--;
+                }
+                maximal=max(maximal,range);
+            }
+        }
+        return maximal;
+    }
+};
